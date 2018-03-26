716e88e003de56df4f3b4e6a4dca53bc6e942e7f
Alex Noble and Dennise Ortega <dennise@>
2018-03-26 15:41:56 -0700

    make unique words function work

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 var sentence;
 var words=[];
 var longWords=[];
+var counter=0;
+var found;
+var uniqueWords=[];
+var wordScores=[];
+var wordScore=0;
+
+
 
 $(document).ready(function() {
   $("button#add").click(function(){
     sentence=$("#item1").val();
     words=sentence.split(" ");
-    words.forEach (function(word){
-      if(word.length >=3){
-        longWords.push(word);
-      } else{}
+    words.forEach (function(word1){
+      if(uniqueWords.indexOf(word1)==-1)
+      {
+        uniqueWords.push(word1);
+        //wordScore=1;
+      }
+      else{
+        //wordScore++;
+      }
     });
-    longWords.reverse();
-    alert(longWords.join(''));
+    alert(uniqueWords);
+    //alert(wordScores);
   });
 });
